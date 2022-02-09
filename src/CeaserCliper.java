745ad008c9f18c5a41e94ad066aa72de6223f21b
stepawka62 <Stepawka6218@gmail.com>
2022-02-09 13:12:53 +0300

    fix method encrypted and add deEncrypted method

--- src/CeaserCliper.java
+++ src/CeaserCliper.java
@@ -1,34 +1,35 @@
 public class CeaserCliper {
 
-    private static final String ALPHABET = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    private static final String ALPHABET = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 
 
     public String encrypted(String message, int key) { // кеу = 3.
 
-
         StringBuilder result = new StringBuilder(); //экономит ресурсы
-
 
             //Привет
         for (char aChar : message.toCharArray()) {
 
             int origAlphabetPosition = ALPHABET.indexOf(aChar); //номер буквы в алфавите (начальный)
+            int newAlphabetPosition;// новая индекс зашиврованного символа.
+            char newCharacter = 0;// новый символ (зашиврованный).
 
-            int newAlphabetPosition = (origAlphabetPosition + key) % (ALPHABET.length() / 2); // смещаем на значение KEY.
-
-            char newCharapter = ALPHABET.charAt(newAlphabetPosition); // находит новый символ с новой позицией (индекс)
-
-            result.append(newCharapter); //собираем новое слово из символов  (преобразует в StringBuilder)
-
+            if (origAlphabetPosition > 0) {
+                if (key > 0) {
+                    newAlphabetPosition = (origAlphabetPosition + key) % (ALPHABET.length() / 2);
+                } else {
+                    int newKey = key % (ALPHABET.length() / 2);
+                    newAlphabetPosition = (origAlphabetPosition + (ALPHABET.length() / 2) + newKey) % ALPHABET.length();
+                }
+                newCharacter = ALPHABET.charAt(newAlphabetPosition); // находит новый символ с новой позицией (индекс)
+            }
+            result.append(newCharacter); //собираем новое слово из символов  (преобразует в StringBuilder)
         }
-
-
-
-
-
         return result.toString();
   }
 
-  public String deEncrypted(String message, int key) {return null; }
+  public String deEncrypted(String message, int key) {
+        return encrypted(message, key * (-1));
+    }
 
 }
