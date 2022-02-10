f7d8796662116a4fc6b238a8cc95f3a1f7999c69
stepawka62 <Stepawka6218@gmail.com>
2022-02-10 13:58:50 +0300

    add implementation method choise 3

--- src/ChoiceThree.java
+++ src/ChoiceThree.java
@@ -1,5 +1,46 @@
+import java.awt.image.ImagingOpException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class ChoiceThree {
-    public static void choiceThree() {
+
+    public static void choiceThree() throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ВВедите полный путь к файлу, для его расшифровки.");
+        String pathEncryptedFile = scanner.nextLine();
+
+        System.out.println("Введите полный путь к фалйу, в который записать расшифрованный текст.");
+        String pathNotEncryptedFile = scanner.nextLine();
+
+        CeaserCliper ceaserCliper = new CeaserCliper();
+
+        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(pathEncryptedFile));
+             BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(pathNotEncryptedFile));
+             ) {
+            StringBuilder stringBuilder = new StringBuilder();
+
+           while (bufferedReader.ready()) {
+               String string = bufferedReader.readLine();
+               stringBuilder.append(string).append(System.lineSeparator());
+           }
+            for (int i = 0; i < ceaserCliper.maxSize; i++) { //key = 10
+               String deEncrypted =  ceaserCliper.deEncrypted(stringBuilder.toString(), i);
+            }
+
+
+
+        }
+
+
+
+
+
 
     }
 }
