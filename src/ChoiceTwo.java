f71dc196fd3566753299dec19b7cab1183204652
stepawka62 <Stepawka6218@gmail.com>
2022-02-10 13:23:12 +0300

    add implementation method choise 2

--- src/ChoiceTwo.java
+++ src/ChoiceTwo.java
@@ -1,5 +1,44 @@
-public class ChoiceTwo {
-    public static void choiceTwo() {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
+public class ChoiceTwo {
+    public static void choiceTwo() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите полный путь к файлу, для его расшифровки.");
+        String pathEncryptedFile = scanner.nextLine();
+
+        System.out.println("Введите ключ шифровния.");
+        int key = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Введите полный путь к файлу, в который записать расшифрованный файл.");
+        String pathNotEncryptedFile = scanner.nextLine();
+
+        CeaserCliper ceaserCliper = new CeaserCliper();
+
+        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(pathEncryptedFile));
+             BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(pathNotEncryptedFile))
+        ) {
+            while (bufferedReader.ready()) {
+                String string = bufferedReader.readLine();
+                String deEncryptedString = ceaserCliper.deEncrypted(string, key);
+                bufferedWriter.write(deEncryptedString + System.lineSeparator());
+            }
+        }
+        System.out.println("Содержимое файла расшифровано.");
     }
 }
+
+
+
+
+
+
+
+
+
+
